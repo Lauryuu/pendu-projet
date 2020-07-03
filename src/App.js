476a7e9bfa7063cec8597378bae33a15a7a0f6f9
LaurentGarreau <kuroinamida2009@hotmail.fr>
2020-07-03 09:42:42 +0200

    exercice pendu react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [vies, setVie] = useState(5)
+    const allWords = ["HEROS", "POUVOIR", "MUTANT", "CATASTROPHE", "COSMIQUE", "ENTITE", "XENOMORPHE"]
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    let oneWord = Math.floor(Math.random() * allWords.length)
+    oneWord = allWords[oneWord]
+    const word = oneWord.split('')
+
+    function writeStars(vies) {
+        const stars = []
+        for (let i = 0; i < vies; i++) {
+            stars.push("*")
+        }
+        return stars
+    }
+
+    function less() {
+
+    }
+
+    function createHiddenWords() {
+        const hiddenWord = []
+        for (let i = 0; i < word.length; i++) {
+            hiddenWord.push(" _ ")
+        }
+        return hiddenWord
+    }
+
+    function createButton() {
+        const button = []
+        for (let i = 0; i < alphabet.length; i++) {
+            button.push(<button className="letter" value={alphabet[i]} onClick={disableButton}>{alphabet[i]}</button>)
+        }
+        return button
+    }
+
+    function disableButton(e){
+        const button = e.target
+        button.disabled=true;
+    }
+
+    
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p className="vie">Vies : {writeStars(5)}</p>
+                <div className="word">
+                    {createHiddenWords()}
+                </div>
+                <div className="letter">
+                    {createButton()}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
